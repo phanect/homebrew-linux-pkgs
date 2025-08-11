0488df305947a12a826b36d5761856347027c2ee
Jumpei Ogawa <git@phanective.org>
2025-08-11 20:07:29 +0900

    feat: move Celler directory outside each Homebrew versions folder

--- debs/homebrew/etc/profile.d/homebrew.sh
+++ debs/homebrew/etc/profile.d/homebrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PKG_VERSION="$(cat "/usr/lib/homebrew/package-version.txt")"
-HOMEBREW_BIN="${HOME}/.local/share/homebrew/${PKG_VERSION}/bin"
+HOMEBREW_BIN="${HOME}/.local/share/homebrew/versions/${PKG_VERSION}/bin"
 
 if [[ "${PATH}" != *"${HOMEBREW_BIN}"* ]]; then
   export PATH="$PATH:${HOMEBREW_BIN}"
