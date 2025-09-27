51b49781fd1df2f8c3587b48e643ebd1806fbf76
Jumpei Ogawa <git@phanective.org>
2025-09-27 18:18:32 +0900

    fixup! feat: add snapcraft.yaml

--- scripts/build-snap.sh
+++ scripts/build-snap.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
 
+set -eux
+
+type snap > /dev/null
+type snapcraft > /dev/null
 
 snap set snapcraft provider=multipass
+snapcraft pack
+snap install ./brew_4.6.14_amd64.snap --dangerous --devmode
